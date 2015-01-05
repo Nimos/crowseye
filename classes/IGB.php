da0bd89a45e39ebb431172762651a95f0dba3c5c
Simon <simon@nimos.ws>
2015-01-05 04:21:22 +0100

    fixed bug with eve igb headers

--- classes/IGB.php
+++ classes/IGB.php
@@ -41,7 +41,7 @@
                     { 
                         if (substr($name, 0, 5) == 'HTTP_') 
                         { 
-                            $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value; 
+                            $headers[str_replace(' ', '-', str_replace('_', ' ', substr($name, 5)))] = $value; 
                         } 
                     } 
                     return $headers; 
